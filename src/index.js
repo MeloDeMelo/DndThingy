15413b90c4e25b7e6bfd9f17b1a7a6793d69cfed
Max DeMelo <max.demelo2267@gmail.com>
2018-02-07 17:23:55 -0500

    Added a header to list as well as button to remove list dynamically

--- src/index.js
+++ src/index.js
@@ -3,42 +3,63 @@ import printMe from "./print.js"
 import {ApiClient} from "./request.js"
 
 function component() {
-  let element = document.createElement('div');
-  let button = document.createElement('button');
   let client = new ApiClient();
+  let element = document.createElement('div');
+  let spellsButton = document.createElement('button');
+  let removeListButton = document.createElement('button');
+  let spellListID = 1234;
+  let spellListHeadingID = 2345;
 
   element.innerHTML = ["Hello", "webpack"].join(" ");
-  button.innerHTML = "Get List";
-  button.addEventListener("click", () => {
+  spellsButton.innerHTML = "Get List";
+  removeListButton.innerHTML = "remove List";
+  spellsButton.addEventListener("click", () => {
     client.getSpellsList().then((response) => {
-        // console.log(JSON.parse(response));
-        // console.log(response);
-        let listContainer = document.createElement("ul");
-        let spells = response.results;
-        for(let key in spells){
-          if(!spells.hasOwnProperty(key)) continue;
+      // console.log(JSON.parse(response));
+      // console.log(response);
+      let listContainer = document.createElement("ul");
+      let spells = response.results;
+      for(let key in spells){
+        if(!spells.hasOwnProperty(key)) continue;
+  
+        let spell = spells[key];
+        let spellItem = document.createElement("li");
+        let spellText = document.createTextNode(spell.name);
+        spellItem.appendChild(spellText);
+  
+        // spellItem.addEventListener("click", () => {
+        //   client.getSpecificSpell(spell.url).then((response) => {
+        //     let heading = document.createElement("h1");
+        //     heading.innerText = response.name;
+        //     let description = document.createElement("p");
+        //     description.innerText = response.desc;
+        //     document.body.appendChild(heading);
+        //     document.body.appendChild(description);
+        //   })
+        // })
+  
+        listContainer.appendChild(spellItem);
+      }
+      listContainer.style = "list-style-type:none";
+      listContainer.setAttribute('id', spellListID);
 
-          let spell = spells[key];
-          let spellItem = document.createElement("li");
-          let spellText = document.createTextNode(spell.name);
-          spellItem.appendChild(spellText);
+      let heading = document.createElement("h1");
+      heading.innerText = "Spell List";
+      heading.setAttribute('id', spellListHeadingID);
 
-          // spellItem.addEventListener("click", () => {
-          //   client.getSpecificSpell(spell.url).then((response) => {
-          //     let heading = document.createElement("h1");
-          //     heading.innerText = response.name;
-          //     let description = document.createElement("p");
-          //     description.innerText = response.desc;
-          //   })
-          // })
-
-          listContainer.appendChild(spellItem);
-        }
-        document.body.appendChild(listContainer);
-    })
+      document.body.appendChild(heading);
+      document.body.appendChild(listContainer);
+    });
+  });
+  removeListButton.addEventListener("click", () => {
+    var element = document.getElementById(spellListID);
+    element.parentNode.removeChild(element);
+    element = document.getElementById(spellListHeadingID);
+    element.parentNode.removeChild(element);
   });
   element.appendChild(document.createElement("br"));
-  element.appendChild(button);
+  element.appendChild(spellsButton);
+  element.appendChild(removeListButton);
 
   return element;
 }
